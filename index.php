49e430e1229088421d4bc0d63b5e0441b2f4d794
Federico Barbieri <federicobarbieri54@gmail.com>
2023-12-20 22:12:58 +0100

    static html to wordpress

--- index.php
+++ index.php
@@ -1,16 +1,51 @@
-<?php get_header();
+<?php get_header(); ?>
 
+<div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/library-hero.jpg') ?>)"></div>
+      <div class="page-banner__content container t-center c-white">
+        <h1 class="headline headline--large">Welcome!</h1>
+        <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
+        <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
+        <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+      </div>
+    </div>
 
-while(have_posts()){
-    the_post(); ?>
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <div class="hero-slider">
+      <div data-glide-el="track" class="glide__track">
+        <div class="glide__slides">
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/bus.jpg') ?>">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">Free Transportation</h2>
+                <p class="t-center">All students have free unlimited bus fare.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/apples.jpg') ?>">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">An Apple a Day</h2>
+                <p class="t-center">Our dentistry program recommends eating apples.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/bread.jpg') ?>">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">Free Food</h2>
+                <p class="t-center">Fictional University offers lunch plans for those in need.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
+      </div>
+    </div>
 
-    <?php the_content(); ?>
-
-
-    <?php    }
-
-get_footer();
+<?php get_footer();
 
 ?>
 
